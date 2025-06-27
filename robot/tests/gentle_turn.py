69f4700d4f6b75f735afbc6e62a69d827b8e0895
Doug Blanding <dblanding@gmail.com>
2025-06-27 18:38:33 -0400

    add robot/tests/gentle_turn.py and robot/tests/spin_robot.py Run pyinfra inventory.py deploy/update_code.py

---
+++ robot/tests/gentle_turn.py
@@ -0,0 +1,4 @@
+left_motor.enable()
+left_motor.speed(0.4)
+right_motor.enable()
+right_motor.speed(0.8)
---
+++ robot/tests/spin_robot.py
@@ -0,0 +1,4 @@
+left_motor.enable()
+left_motor.speed(0.8)
+right_motor.enable()
+right_motor.speed(-0.8)
