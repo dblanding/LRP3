586f0fa4b875f3ad58879104f38530dbbea99726
Doug Blanding <dblanding@gmail.com>
2025-07-14 19:56:38 -0400

    problem on page 11, chapter 11 resolved.

--- robot/inventor_hat_service.py
+++ robot/inventor_hat_service.py
@@ -62,16 +62,10 @@ def reset_encoders(*_):
     right_encoder.zero()
 
 def update_encoders(client):
-    left_data = left_encoder.capture()
-    right_data = right_encoder.capture()
-    publish_json(
-        client,
-        "sensors/encoders/data",
+    publish_json(client, "sensors/encoders/data",
         {
-            "left_distance": left_data.radians * wheel_radius,
-            "right_distance": right_data.radians * wheel_radius,
-            "left_mm_per_sec": left_data.radians_per_second * wheel_radius,
-            "right_mm_per_sec": right_data.radians_per_second * wheel_radius,
+            "left_count": left_encoder.count(),
+            "right_count": right_encoder.count()
         }
     )
 
@@ -97,13 +91,13 @@ client.message_callback_add("motors/servo/tilt/position", partial(set_servo_posi
 client.message_callback_add("motors/servo/tilt/stop", partial(stop_servo, tilt))
 client.message_callback_add("all/stop", stop_motors)
 client.message_callback_add("all/#", all_messages)
-client.message_callback_add("sensors/encoders/ control/reset", reset_encoders)
+client.message_callback_add("sensors/encoders/control/reset", reset_encoders)
 
 client.connect("localhost", 1883)
 board.leds.set_rgb(0, 0, 255, 0)
 client.loop_start()
 while True:
-    #update_encoders(client)
+    update_encoders(client)
     if time.time() - last_message > 1:
         stop_motors()
     time.sleep(0.1)
