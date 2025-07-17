8c33407ca3fb9ef4d78ad1c7a2bfea6ad8d7333f
Doug Blanding <dblanding@gmail.com>
2025-07-17 06:34:47 -0400

    up to ch 11, p35, just before "Putting config controls on the web pages"

--- robot/wheel_control_service.py
+++ robot/wheel_control_service.py
@@ -11,8 +11,8 @@ class WheelControlService:
     def __init__(self):
         self.left_speed = 0
         self.right_speed = 0
-        self.left_pid = PIDController(0.001, 0.01)
-        self.right_pid = PIDController(0.001, 0.01)
+        self.left_pid = PIDController(0.005, 0.005)
+        self.right_pid = PIDController(0.005, 0.005)
         self.time_stepper = TimeStepper()
         self.enabled = False
 
@@ -47,11 +47,23 @@ class WheelControlService:
 
         publish_json(client, "motors/wheels", [left_control, right_control])
 
+    def on_config_updated(self, client, userdata, message):
+        data = json.loads(message.payload)
+        self.left_pid.handle_config_messages(data, 'wheel_control')
+        self.right_pid.handle_config_messages(data, 'wheel_control')
+
     def start(self):
         client = connect(start_loop=False)
         client.subscribe("sensors/encoders/data")
         client.message_callback_add("sensors/encoders/data",
                                     self.on_encoders_data)
+        client.subscribe("config/updated")
+        client.message_callback_add("config/updated",
+                                    self.on_config_updated)
+        publish_json(client, "config/get", [
+                        "wheel_control/proportional",
+                        "wheel_control/integral"])
+
         client.subscribe("wheel_control/#")
         client.message_callback_add("wheel_control/enabled",
                                     self.on_enabled)
