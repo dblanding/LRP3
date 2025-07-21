b4e7d3924cb68600c16f5a2257f67ce272b9a57a
Doug Blanding <dblanding@gmail.com>
2025-07-21 14:11:49 -0400

    OK up through magnetometer

---
+++ robot/tests/test_bno055.py
@@ -0,0 +1,12 @@
+from adafruit_extended_bus import ExtendedI2C as I2C
+import adafruit_bno055
+import time
+
+i2c = I2C(2)
+bno055 = adafruit_bno055.BNO055_I2C(i2c)
+
+while True:
+    # print(bno055.temperature)
+    # print("Gyro Calibration:", bno055.calibration_status[1], "Gyro:", bno055.gyro)
+    print("Acc Calibration:", bno055.calibration_status[2], "Acc:", bno055.acceleration)
+    time.sleep(0.1)
