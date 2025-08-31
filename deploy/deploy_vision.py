7dbb32edaf9d0a9f97dc3f073c8391a765fdcde0
Doug Blanding <dblanding@gmail.com>
2025-08-31 17:28:35 -0400

    add code to display console/inspection symbol on web pages. Also small change in deploy_vision.py

--- deploy/deploy_vision.py
+++ deploy/deploy_vision.py
@@ -1,4 +1,5 @@
-from pyinfra.operations import apt, files
+from deploy import virtual_env
+from pyinfra.operations import apt, files, pip
 
 apt.packages(
     name="Install AI packages",
@@ -8,9 +9,14 @@ apt.packages(
     _sudo=True,
 )
 
+pip.packages(
+    name="Install OpenCV contrib",
+    packages=["opencv-contrib-python"],
+    virtualenv=virtual_env.robot_venv,
+    )
+
 files.download(
     name="Download Face detection model",
     src="https://github.com/opencv/opencv_zoo/raw/refs/heads/main/models/face_detection_yunet/face_detection_yunet_2023mar_int8.onnx",
     dest="face_detection_yunet_2023mar_int8.onnx"
 )
-
