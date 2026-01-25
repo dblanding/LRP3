33672222f1b2e0e78fe5c5916e5ff38a9e5567de
Doug Blanding <dblanding@gmail.com>
2026-01-25 16:44:13 -0500

    Through "Building a Pose System" in ch12 (revised)

--- robot/common/poses.py
+++ robot/common/poses.py
@@ -1,16 +1,7 @@
 import numpy as np
 
+class Poses(np.ndarray):
+    Pose = np.dtype([('x', np.float32), ('y', np.float32), ('theta', np.float32)])
 
-def rotated_poses(poses, rotation):
-    return np.column_stack((
-            poses[:, 0],
-            poses[:, 1],
-            poses[:, 2] + rotation
-    ))
-
-def translated_poses(poses, length):
-    return np.column_stack((
-            poses[:, 0] + np.cos(poses[:, 2]) * length,
-            poses[:, 1] + np.sin(poses[:, 2]) * length,
-            poses[:, 2]
-    ))
+    def __new__(cls, input_array):
+        return np.asarray(input_array, dtype=cls.Pose).view(cls)
